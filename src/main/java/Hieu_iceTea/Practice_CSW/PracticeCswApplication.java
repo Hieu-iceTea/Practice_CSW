0959654539fa566df74de680c92f484a6493b4be
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-08-07 09:32:53 +0700

    Intinial

---
+++ src/main/java/Hieu_iceTea/Practice_CSW/PracticeCswApplication.java
@@ -0,0 +1,13 @@
+package Hieu_iceTea.Practice_CSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeCswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeCswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hieu_iceTea/Practice_CSW/PracticeCswApplicationTests.java
@@ -0,0 +1,13 @@
+package Hieu_iceTea.Practice_CSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeCswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
